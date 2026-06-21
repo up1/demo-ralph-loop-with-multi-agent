62089d24cc178bdb24dc50b278d4dd271683d836
up1 <somkiat.p@gmail.com>
2026-06-21 21:20:40 +0700

    Remove all files

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app.js');
-
-const PORT = 3000;
-
-const server = app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
-
-module.exports = server;
