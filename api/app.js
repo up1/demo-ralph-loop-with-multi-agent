17573fb15db3f5a6a5251e191a92b233f054a870
up1 <somkiat.p@gmail.com>
2026-06-21 21:14:33 +0700

    Remove all

--- api/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/hello', (req, res) => {
-  res.json({ message: 'Hello, World!' });
-});
-
-module.exports = app;
--- api/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-
-const PORT = 3000;
-
-const server = app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
-
-module.exports = server;
